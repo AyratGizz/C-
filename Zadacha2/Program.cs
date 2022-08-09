33e4fe721a384cfc2b317c1248e654811698cb12
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-10 03:22:28 +0500

    ver.1.0.3

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿// ***Введите два числа и определите какое больше***
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > number1)
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(max = number2);
+}
+else 
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(max = number1);
+}

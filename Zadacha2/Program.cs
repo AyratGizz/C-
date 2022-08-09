79a9568f91cc9119e02ed87fd256387c68bfd5fc
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-10 03:41:10 +0500

    ver.1.0.4

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,16 +1,22 @@
 ﻿Console.WriteLine("Введите два числа и программа определит какое больше!");
+
 Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int max = number1;
+
 if (number2 > number1)
 {
     Console.Write("Большое число: ");
-    Console.Write(max = number2);
+    Console.WriteLine(number2);
+    Console.Write("Меньшое число: ");
+    Console.Write(number1);
 }
 else 
 {
     Console.Write("Большое число: ");
-    Console.Write(max = number1);
+    Console.WriteLine(number1);
+    Console.Write("Меньшое число: ");
+    Console.Write(number2);
 }

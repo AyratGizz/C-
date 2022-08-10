e7a91c305139e3d76f8f689d2de6fb5e40aaa9de
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-10 23:21:56 +0500

    ver.2.0

--- Zadacha4/Program.cs
+++ Zadacha4/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number1 > number2) max = number1;
+if (number2 > number3) max = number2;
+if (number3 > number2) max = number3;
+
+Console.Write("Большее число: ");
+Console.Write(max);
+

6dd7045ad245dccea488e4a6d83070b1682d01c0
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-10 23:45:51 +0500

    End_Commit

--- Zadacha6/Program.cs
+++ Zadacha6/Program.cs
@@ -1,7 +1,8 @@
 ﻿Console.Write ("Введите любое число и программа определит чётноё оно или не чётное: ");
 
-int number = Convert.ToInt32 (Console.Read());
-if (number % 2 == 0) Console.WriteLine ("Число чётное");
-else Console.WriteLine ("Число нечётное");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number % 2 == 0) 
+Console.WriteLine ("Введённое число чётное");
+else Console.WriteLine ("Введённое число нечётное");
 
 

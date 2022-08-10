bf78f97093c03f815e1390ca61af14c4369cf14c
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-10 12:10:37 +0500

    ver.1.0.6

--- Zadacha6/Program.cs
+++ Zadacha6/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите любое число: ");
+int number = Convert.ToInt32 (Console.Read());
+if (number % 2 == 0) Console.WriteLine ("Число чётное");
+else Console.WriteLine ("Число нечётное");
+
+

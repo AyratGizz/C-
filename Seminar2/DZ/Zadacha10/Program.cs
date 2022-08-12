133fb7f40b5a45c74adfc7548d1b165ab101da85
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-12 13:33:55 +0500

    ver.3.0.0.

---
+++ Seminar2/DZ/Zadacha10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Программа принимает на вход трехзначное число и выводит вторую цифру числа!");
+
+Console.Write("Введи трёхзначное число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+Console.WriteLine("Вторая цифра этого числа: "+stringNumber[1]);
---
+++ Seminar2/Program.cs
@@ -0,0 +1,64 @@
+﻿// ****Программа которая выводит случайное число из отрезка [10, 90] и показывает наибольшую цифру числа.
+// void GetMaxNumberRandomValue()
+// {
+//     int randomValue = new Random().Next(10, 100);
+
+//     Console.WriteLine($"Random value - {randomValue}");
+
+//     int lastNumber = randomValue % 10;
+//     int firstNumber = randomValue / 10;
+
+//     int max = lastNumber;
+
+//     if (lastNumber == firstNumber)
+//     {
+//         Console.WriteLine("Числа равны!");
+//     }
+//     else
+//     {
+//         if (max < firstNumber)
+//         {
+//             max = firstNumber;
+//         }
+
+//         Console.WriteLine($"MAX = {max}");
+//     }
+// }
+// GetMaxNumberRandomValue();
+
+//---------------------------------------------------------------------------------
+//*** Программа которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+
+//Console.WriteLine("Определяется рандомное число");
+
+//int number = Convert.ToInt32(Console.ReadLine());
+// Random random = new Random();
+// int number = random.Next(100, 1000);
+// Console.Write(number);
+// int number2 = number % 10;
+// int number3 = number / 100;
+// Console.Write($" {number3}{number2} ");
+
+//---------------------------------------------------------------------------------
+//***
+
+// void GoNumbers (int num1, int num2)
+// {
+//     if (num2 % num1 == 0)
+//     {
+//         Console.WriteLine("Второе число кратно перовму");
+//     }
+//     else Console.WriteLine(num2 % num1);
+// }
+
+// Console.Write ("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе чило: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+// GoNumbers(num1, num2);
+
+//-----------------------------------------------------------------------------------
+//***
+

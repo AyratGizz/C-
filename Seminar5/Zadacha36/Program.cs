450aaa03ab953ea2194fb6b8b53684085972fbc5
AyratGizz <gizzatullin-airat@mail.ru>
2022-08-20 02:21:30 +0500

    ver.3.1.5

--- Seminar5/Zadacha36/Program.cs
+++ Seminar5/Zadacha36/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов в массиве: ");
+int len = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное число в массиве: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число в массиве: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[len];
+
+int[] FillArray(int[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(min, max);
+    }
+    return arr;
+}
+Console.Write("Случайный массив: ");
+Console.Write("[");
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write("]");
+}
+
+int SumNumbersOddPlaces (int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 !=1)
+        {
+        sum = sum + arr[i];
+        }
+    }
+    return sum;
+
+}
+
+int[] result = FillArray(array);
+PrintArr(array);
+Console.WriteLine();
+int SummaNumOddPlaces = SumNumbersOddPlaces(array);
+Console.Write("Сумма элементов массива, стоящих на нечётных позициях = ");
+Console.WriteLine(SummaNumOddPlaces);
--- Seminar5/Zadacha38/Program.cs
+++ Seminar5/Zadacha38/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов в массиве: ");
+int lenN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное число в массиве: ");
+int minN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число в массиве: ");
+int maxN = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[lenN];
+
+double[] FillArray(double[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(minN, maxN);
+    }
+    return arr;
+}
+Console.Write("Случайный массив: ");
+Console.Write("[");
+void PrintArr(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write("]");
+}
+
+double CheckMinMax(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max) 
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min) 
+        {
+            min = arr[i];
+        }
+    }
+    double summa = max - min;
+    return summa;
+}
+
+double[] result = FillArray(array);
+PrintArr(array);
+Console.WriteLine();
+double Summa = CheckMinMax (array);
+Console.Write($"Pазницу между максимальным и минимальным элементом массива = ");
+Console.WriteLine(Summa);
